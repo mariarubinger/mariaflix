4eb9c7b6fec4ebe38c474b79b266adc0f19a58d3
Maria Rubinger <rubinger.maria@gmail.com>
2020-08-06 02:02:40 -0300

    Criação pasta hooks, alteração Pages cadastro Categoria, dados inicias

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Menu from'../../components/Menu';
+import Menu from '../../components/Menu';
 import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
@@ -10,26 +10,27 @@ function Home() {
     <div style={{ background: "#141414" }}>
       <Menu />
 
-      <BannerMain 
-      videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-      videoDescription={"Seja bem-vinda ao MariaFlix, conteúdos para você que está iniciando na Programação!"}
-      url={dadosIniciais.categorias[0].videos[0].url}/>
-      
-      <Carousel
-      ignoreFirstVideo
-      category={dadosIniciais.categorias[0]}
-      />
-      <Carousel
-      category={dadosIniciais.categorias[1]}
-      />
-      <Carousel
-      category={dadosIniciais.categorias[2]}
-      />
-      <Carousel
-      category={dadosIniciais.categorias[3]}
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        videoDescription="Seja bem-vindo(a) ao MariaFlix!"
+        url={dadosIniciais.categorias[0].videos[0].url}
       />
 
-      <Footer/>
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />
+      <Carousel
+        category={dadosIniciais.categorias[3]}
+      />
+
+      <Footer />
     </div>
   );
 }
